346636e1b218f4cf483f0e7add71db0455fac527
antelmor <andres.tellez.m13@gmail.com>
2023-11-01 18:01:12 -0400

    Fixed importing error

--- aiida_tb2j/data/__init__.py
+++ aiida_tb2j/data/__init__.py
@@ -1,9 +1,7 @@
 from .exchange import ExchangeData
-from .projectors import HubbardData
 from .curie import CurieData
 
 __all__ = (
     'ExchangeData',
-    'HubbardData',
-    'CurieData'
+    'HubbardData'
 )
--- aiida_tb2j/workflows/__init__.py
+++ aiida_tb2j/workflows/__init__.py
@@ -1,5 +1,4 @@
 from .siesta import TB2JSiestaWorkChain
-from .hubbard import SiestaDFTUWorkChain
 from .dmi import DMIWorkChain
 from .orientation import MagneticOrientationWorkChain
 from .ground_state import GroundStateWorkChain
@@ -7,6 +6,5 @@ from .ground_state import GroundStateWorkChain
 __all__ = (
     'DMIWorkChain',
     'GroundStateWorkChain',
-    'SiestaDFTUWorkChain',
     'TB2JSiestaWorkChain'
 )
